6efb6b19ab3e873af6ab333808f04d85c974c810
aric.zheng <1290657123@qq.com>
2025-10-29 20:02:27 +0800

    feat: upt demo

--- packages/example/src/app.tsx
+++ packages/example/src/app.tsx
@@ -1,23 +1,27 @@
 import ReactCkeditor from '@jswork/react-ckeditor/src';
-import '@jswork/react-ckeditor/src/style.scss';
-import {useEffect, useState} from "react";
+// import '@jswork/react-ckeditor/src/style.scss';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [value, setValue] = useState(`<p>Hello React Ckeditor!</p>`);
 
   useEffect(() => {
     setTimeout(() => {
-      console.log('1s later update')
+      console.log('1s later update');
       setValue(`<p>Hello React Ckeditor! Updated!</p>`);
     }, 1000);
   }, []);
 
   return (
     <div className="m-10 p-4 shadow bg-gray-100 text-gray-800 hover:shadow-md transition-all">
-      <div className="badge badge-warning absolute right-0 top-0 m-4">
-        Build Time: {BUILD_TIME}
-      </div>
-      <ReactCkeditor value={value} onChange={(e) => setValue(e.target.value)}/>
+      <div className="badge badge-warning absolute right-0 top-0 m-4">Build Time: {BUILD_TIME}</div>
+      <ReactCkeditor
+        value={value}
+        onChange={(e) => {
+          console.log('value: ', e.target.value);
+          setValue(e.target.value);
+        }}
+      />
     </div>
   );
 }
